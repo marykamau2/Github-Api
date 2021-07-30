332d62cea1e756f1e74c66a1407776f0944a4f76
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 13:53:55 +0300

    generated a component github detailes

--- API/src/app/github-details/github-details.component.spec.ts
+++ API/src/app/github-details/github-details.component.spec.ts
@@ -1,28 +1,28 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-// import { GithubDetailsComponent } from './github-details.component';
+import { GithubDetailsComponent } from './github-details.component';
 
-// describe('GithubDetailsComponent', () => {
-//   let component: GithubDetailsComponent;
-//   let fixture: ComponentFixture<GithubDetailsComponent>;
+describe('GithubDetailsComponent', () => {
+  let component: GithubDetailsComponent;
+  let fixture: ComponentFixture<GithubDetailsComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ GithubDetailsComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GithubDetailsComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(GithubDetailsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GithubDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
