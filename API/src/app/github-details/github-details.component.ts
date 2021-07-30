267f8a37a8e667648ee2691f3903a7e6799adb85
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 13:58:15 +0300

    generated the user interface

--- API/src/app/github-details/github-details.component.ts
+++ API/src/app/github-details/github-details.component.ts
@@ -1,28 +1,28 @@
-// import { Repository } from './../repository';
-// import { HttpClient } from '@angular/common/http';
-// import { User } from './../user';
-// import { SearchGithubService } from './../search-github.service';
-// import { Component, OnInit } from '@angular/core';
+import { Repository } from './../repository';
+import { HttpClient } from '@angular/common/http';
+import { User } from './../user';
+import { SearchGithubService } from './../search-github.service';
+import { Component, OnInit } from '@angular/core';
 
 
-// @Component({
-//   selector: 'app-github-details',
-//   templateUrl: './github-details.component.html',
-//   styleUrls: ['./github-details.component.css']
-// })
-// export class GithubDetailsComponent implements OnInit {
+@Component({
+  selector: 'app-github-details',
+  templateUrl: './github-details.component.html',
+  styleUrls: ['./github-details.component.css']
+})
+export class GithubDetailsComponent implements OnInit {
 
-//   user!: User;
-//   searchGithubService:SearchGithubService;
-//   repoDetails : any= [];
+  user!: User;
+  searchGithubService:SearchGithubService;
+  repoDetails : any= [];
 
-//   constructor(searchGithubService: SearchGithubService) {
-//     this.searchGithubService = searchGithubService;
-//    }
+  constructor(searchGithubService: SearchGithubService) {
+    this.searchGithubService = searchGithubService;
+   }
 
-//   ngOnInit() {
-//     this.user = this.searchGithubService.user;
-//     this.repoDetails = this.searchGithubService.repoData
-//    }
+  ngOnInit() {
+    this.user = this.searchGithubService.user;
+    this.repoDetails = this.searchGithubService.repoData
+   }
 
-// }
+}
