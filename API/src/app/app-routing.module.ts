764868f6a4a070b6bb7ab780cac350aa5843c820
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 14:01:03 +0300

    added the app module

--- API/src/app/app-routing.module.ts
+++ API/src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GithubDetailsComponent } from './github-details/github-details.component';
 import { GithubFormComponent } from './github-form/github-form.component';
 
-// const routes: Routes = [
-//   {path:'github-form', component:GithubFormComponent},
-//   {path:'github-details', component:GithubDetailsComponent}
-// ];
+const routes: Routes = [
+  {path:'github-form', component:GithubFormComponent},
+  {path:'github-details', component:GithubDetailsComponent}
+];
 
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
