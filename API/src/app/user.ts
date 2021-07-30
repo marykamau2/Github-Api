df7f0df10f58f33ee6f2c5fbb7c282bbd012b309
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 14:13:48 +0300

    initialize class user

--- API/src/app/user.ts
+++ API/src/app/user.ts
@@ -1,11 +1,11 @@
-// export class User {
-//     constructor(
-//         public bio: string,
-//         public public_repos: number,
-//         public login: string,
-//         public avatar_url: string,
-//         public created_at: Date) {
+export class User {
+    constructor(
+        public bio: string,
+        public public_repos: number,
+        public login: string,
+        public avatar_url: string,
+        public created_at: Date) {
 
-//     }
+    }
 
-// }
+}
