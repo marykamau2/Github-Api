ab83077053d4db2173b53c2247b9fe3ef5db688a
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 14:09:34 +0300

    initialize class repository

--- API/src/app/repository.ts
+++ API/src/app/repository.ts
@@ -1,11 +1,11 @@
-// export class Repository {
-//     constructor(
-//         public name:string,
-//         public description:string,
-//         public updated_at:Date,
-//         public clone_url:string,
-//         public language:string
-//     ){
+export class Repository {
+    constructor(
+        public name:string,
+        public description:string,
+        public updated_at:Date,
+        public clone_url:string,
+        public language:string
+    ){
 
-//     }
-// }
+    }
+}
