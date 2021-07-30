3f72c31db63a85eb561a5bb067e3363c7aaeb6da
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 14:05:40 +0300

    created routes

--- API/src/app/app.module.ts
+++ API/src/app/app.module.ts
@@ -1,33 +1,33 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-// import{HttpClientModule} from '@angular/common/http';
-// import { FormsModule } from '@angular/forms'
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { GithubDetailsComponent } from './github-details/github-details.component';
-// import { GithubFormComponent } from './github-form/github-form.component';
-// import { HighlightDirective } from './highlight.directive';
-// import { DateCountPipe } from './date-count.pipe';
+import{HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubDetailsComponent } from './github-details/github-details.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     GithubDetailsComponent,
-//     GithubFormComponent,
-//     HighlightDirective,
-//     DateCountPipe,
-//       GithubDetailsComponent
-//    ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//     HttpClientModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubDetailsComponent,
+    GithubFormComponent,
+    HighlightDirective,
+    DateCountPipe,
+      GithubDetailsComponent
+   ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
