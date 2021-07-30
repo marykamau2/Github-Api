5331c6d64ec4f8a3e3d08f79eafae4c9e31912bd
marykamau2 <mary.kamau@student.moringaschool.com>
2021-07-30 14:11:14 +0300

    create search github component

--- API/src/app/search-github.service.spec.ts
+++ API/src/app/search-github.service.spec.ts
@@ -1,16 +1,16 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { SearchGithubService } from './search-github.service';
+import { SearchGithubService } from './search-github.service';
 
-// describe('SearchGithubService', () => {
-//   let service: SearchGithubService;
+describe('SearchGithubService', () => {
+  let service: SearchGithubService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(SearchGithubService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SearchGithubService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- API/src/app/search-github.service.ts
+++ API/src/app/search-github.service.ts
@@ -1,58 +1,58 @@
 
-// import { Repository } from './repository';
-// import { User } from './user';
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
+import { Repository } from './repository';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class SearchGithubService {
-//   user!: User;
-//   repository!: Repository;
-//   repoData:any = [];
-//   newUserData:any = [];
-//   constructor(private http: HttpClient) {
-//     this.user = new User("", 0, "", "", new Date())
-//     this.repository = new Repository("", "", new Date(), "", "")
-//   }
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchGithubService {
+  user!: User;
+  repository!: Repository;
+  repoData:any = [];
+  newUserData:any = [];
+  constructor(private http: HttpClient) {
+    this.user = new User("", 0, "", "", new Date())
+    this.repository = new Repository("", "", new Date(), "", "")
+  }
 
-//     getUserData(username:string){
-//       interface ApiResponse {
-//         bio: string,
-//         public_repos: number,
-//         login: string,
-//         avatar_url: string,
-//         created_at: Date
+    getUserData(username:string){
+      interface ApiResponse {
+        bio: string,
+        public_repos: number,
+        login: string,
+        avatar_url: string,
+        created_at: Date
 
-//       }
-//       let promise = new Promise<void>((resolve, reject) => {
-//         this.http.get<ApiResponse>("https://api.github.com/users/" + username).toPromise().then(response => {
+      }
+      let promise = new Promise<void>((resolve, reject) => {
+        this.http.get<ApiResponse>("https://api.github.com/users/" + username).toPromise().then(response => {
 
-//           this.user.bio = response.bio;
-//           this.user.login = response.login;
-//           this.user.public_repos = response.public_repos;
-//           this.user.avatar_url = response.avatar_url;
-//           this.user.created_at = response.created_at;
+          this.user.bio = response.bio;
+          this.user.login = response.login;
+          this.user.public_repos = response.public_repos;
+          this.user.avatar_url = response.avatar_url;
+          this.user.created_at = response.created_at;
 
-//           resolve()
+          resolve()
 
-//         }, error => {
-//           reject(error)
-//         })
-//         this.http.get<any>("https://api.github.com/users/" + username + "/repos").toPromise().then(response =>{
-//           for(let i=0;i<response.length;i++){
-//             this.newUserData = new Repository(response[i].name, response[i].description, response[i].updated_at, response[i].clone_url, response[i].language)
-//             this.repoData.push(this.newUserData)
-//           }
-//           resolve()
-//         }, error => {
-//           reject(error)
-//         })
+        }, error => {
+          reject(error)
+        })
+        this.http.get<any>("https://api.github.com/users/" + username + "/repos").toPromise().then(response =>{
+          for(let i=0;i<response.length;i++){
+            this.newUserData = new Repository(response[i].name, response[i].description, response[i].updated_at, response[i].clone_url, response[i].language)
+            this.repoData.push(this.newUserData)
+          }
+          resolve()
+        }, error => {
+          reject(error)
+        })
 
-//       })
-//       return promise;
+      })
+      return promise;
 
-//     }
-// }
+    }
+}
